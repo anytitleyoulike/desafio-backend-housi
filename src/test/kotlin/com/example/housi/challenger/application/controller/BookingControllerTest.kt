96438c3062e1feb0ba96e3efe484fc8f043f608b
Marcello Cordeiro <maarc.hen@gmail.com>
2024-02-08 01:39:00 -0300

    feat: added booking utils calls on BookingControllerTest

--- src/test/kotlin/com/example/housi/challenger/application/controller/BookingControllerTest.kt
+++ src/test/kotlin/com/example/housi/challenger/application/controller/BookingControllerTest.kt
@@ -1,9 +1,5 @@
 package com.example.housi.challenger.application.controller
 
-import com.example.housi.challenger.application.controller.BookingController
-import com.example.housi.challenger.application.entity.Booking
-import com.example.housi.challenger.application.entity.Property
-import com.example.housi.challenger.application.entity.Resident
 import com.example.housi.challenger.application.exception.LocalDateException
 import com.example.housi.challenger.application.usecase.GetUnavailableDateUsecase
 import io.mockk.every
@@ -11,40 +7,19 @@ import io.mockk.mockk
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.jupiter.api.assertThrows
-
-import java.time.LocalDate
+import utils.BookingTestUtils
 
 class BookingControllerTest {
     private val getUnavailableDateUsecase = mockk<GetUnavailableDateUsecase>()
     private val bookingController = BookingController(getUnavailableDateUsecase)
-
+    private val bookings = BookingTestUtils.getBooking()
+    private val listOfBookings = BookingTestUtils.getListOfBookings()
     private val id = "12345"
-    private val bookings = Booking(
-        id=id,
-        checkIn = LocalDate.now(),
-        checkOut = LocalDate.now().plusDays(2),
-        property = Property(
-            id = "abcd",
-            name= "Residencia 1"
-        ),
-        residents = listOf(
-            Resident(userId = "09876",
-                name = "User Test",
-                email = "user@test.com",
-                phone = "99 99999-9999")
-        )
-    )
 
-    val blockedDates = listOf<LocalDate>(
-        LocalDate.parse("2024-02-01"),
-        LocalDate.parse("2024-02-02"),
-        LocalDate.parse("2024-02-03")
-    )
-
+    val blockedDates = BookingTestUtils.getBlockedDates()
     @Test
     fun `Should get bookings by property id`(){
-        val listBooking = listOf(bookings)
-        every { getUnavailableDateUsecase.findById(id) } returns listBooking
+        every { getUnavailableDateUsecase.findById(id) } returns listOfBookings
 
         val bookListMocked = bookingController.getById(id)
 
